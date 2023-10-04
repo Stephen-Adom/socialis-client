ef82fc9856030d375f8bff50e3dc0ac299b0132a
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-10-04 21:35:23 +0000

    created forgot password component

--- src/modules/auth/src/lib/auth.module.ts
+++ src/modules/auth/src/lib/auth.module.ts
@@ -5,9 +5,15 @@ import { authRoutes } from './lib.routes';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { AnimateSvgComponent } from './login/animate-svg/animate-svg.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(authRoutes)],
-  declarations: [RegisterComponent, LoginComponent, AnimateSvgComponent],
+  declarations: [
+    RegisterComponent,
+    LoginComponent,
+    AnimateSvgComponent,
+    ForgotPasswordComponent,
+  ],
 })
 export class AuthModule {}
--- src/modules/auth/src/lib/lib.routes.ts
+++ src/modules/auth/src/lib/lib.routes.ts
@@ -1,8 +1,14 @@
 import { Route } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const authRoutes: Route[] = [
   { path: 'register', pathMatch: 'full', component: RegisterComponent },
   { path: 'login', pathMatch: 'full', component: LoginComponent },
+  {
+    path: 'forgot-password',
+    pathMatch: 'full',
+    component: ForgotPasswordComponent,
+  },
 ];
