e34a25dfb6374f70ba0e26906b0abd115020a986
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-10-05 23:09:47 +0000

    update toaster class replacement

--- src/shared/toasters/src/lib/error-toaster/error-toaster.component.ts
+++ src/shared/toasters/src/lib/error-toaster/error-toaster.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,4 +8,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './error-toaster.component.html',
   styleUrls: ['./error-toaster.component.css'],
 })
-export class ErrorToasterComponent {}
+export class ErrorToasterComponent {
+  @ViewChild('toastDanger') toastDanger!: ElementRef<HTMLDivElement>;
+
+  closeNotification() {
+    if (this.toastDanger.nativeElement.classList.contains('animate-normal')) {
+      this.toastDanger.nativeElement.classList.replace(
+        'animate-normal',
+        'animate-reverse'
+      );
+    }
+  }
+}
