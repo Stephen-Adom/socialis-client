c6e8b2e3e68d8e17d855f799b4937400dabfd4b7
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-10-11 20:44:29 +0000

    create form for making a post

--- src/lib/new-post-modal/src/lib/new-post-modal/new-post-modal.component.ts
+++ src/lib/new-post-modal/src/lib/new-post-modal/new-post-modal.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+type postImageType = {
+  base64: string;
+  file: File;
+  id: number;
+};
 
 @Component({
   selector: 'lib-new-post-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './new-post-modal.component.html',
   styleUrls: ['./new-post-modal.component.css'],
 })
-export class NewPostModalComponent {}
+export class NewPostModalComponent {
+  Form: FormGroup;
+  postImages: postImageType[] = [];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.Form = this.formBuilder.group({
+      content: ['', Validators.required],
+    });
+  }
+
+  uploadImage(event: any) {
+    const file = <File>event.target.files[0];
+  }
+
+  submitPost() {
+    if (this.Form.valid) {
+      console.log(this.Form.value);
+    } else {
+      this.Form.markAllAsTouched();
+    }
+  }
+}
