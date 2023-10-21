efa7782e1dfbfcb6238846f82fc57e7d9c9da106
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-10-21 19:21:12 +0000

    install image cropper

--- src/lib/edit-cover-background-modal/src/lib/edit-cover-background-modal/edit-cover-background-modal.component.ts
+++ src/lib/edit-cover-background-modal/src/lib/edit-cover-background-modal/edit-cover-background-modal.component.ts
@@ -1,11 +1,29 @@
+/* eslint-disable @nx/enforce-module-boundaries */
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ImageCroppedEvent, ImageCropperModule } from 'ngx-image-cropper';
 
 @Component({
   selector: 'lib-edit-cover-background-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ImageCropperModule],
   templateUrl: './edit-cover-background-modal.component.html',
   styleUrls: ['./edit-cover-background-modal.component.css'],
 })
-export class EditCoverBackgroundModalComponent {}
+export class EditCoverBackgroundModalComponent {
+  imageURL =
+    'https://res.cloudinary.com/dt8tdf7uu/image/upload/v1688563062/cld-sample-5.jpg';
+  cropImgPreview!: string | null | undefined;
+
+  cropImg(e: ImageCroppedEvent) {
+    this.cropImgPreview = e.base64;
+  }
+
+  imgLoad(e: ImageCroppedEvent) {
+    console.log(e);
+  }
+
+  imgFailed(e: ImageCroppedEvent) {
+    console.log(e);
+  }
+}
