d1efcd0deef788c131ec074476bd21615c152c20
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-10-23 18:27:06 +0000

    add intl-phone library and complete edit user info form

--- src/lib/edit-auth-profile-modal/src/lib/edit-auth-profile-modal/edit-auth-profile-modal.component.ts
+++ src/lib/edit-auth-profile-modal/src/lib/edit-auth-profile-modal/edit-auth-profile-modal.component.ts
@@ -1,17 +1,30 @@
+/* eslint-disable @nx/enforce-module-boundaries */
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorMessageComponent } from 'auth';
 import {
+  AbstractControl,
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import {
+  CountryISO,
+  NgxIntlTelInputModule,
+  PhoneNumberFormat,
+  SearchCountryField,
+} from 'ngx-intl-tel-input';
 
 @Component({
   selector: 'lib-edit-auth-profile-modal',
   standalone: true,
-  imports: [CommonModule, ErrorMessageComponent, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ErrorMessageComponent,
+    ReactiveFormsModule,
+    NgxIntlTelInputModule,
+  ],
   templateUrl: './edit-auth-profile-modal.component.html',
   styleUrls: ['./edit-auth-profile-modal.component.css'],
 })
@@ -19,14 +32,40 @@ export class EditAuthProfileModalComponent {
   Form: FormGroup;
   validatingUsername = false;
 
+  separateDialCode = false;
+  SearchCountryField = SearchCountryField;
+  CountryISO = CountryISO;
+  PhoneNumberFormat = PhoneNumberFormat.International;
+  preferredCountries: CountryISO[] = [
+    CountryISO.Ghana,
+    CountryISO.UnitedStates,
+    CountryISO.UnitedKingdom,
+  ];
+
   constructor(private formBuilder: FormBuilder) {
     this.Form = this.formBuilder.group({
       firstname: ['', [Validators.required]],
       lastname: ['', [Validators.required]],
       username: ['', [Validators.required]],
       bio: [''],
-      location: [''],
+      address: [''],
       phonenumber: [''],
     });
+  }
+
+  getUsernameErrorMessage(control: AbstractControl<any, any> | null) {
+    if (control?.hasError('usernameExist')) {
+      return 'Username entered already exist';
+    } else {
+      return 'Username is required';
+    }
+  }
+
+  getPhoneErrorMessage(control: AbstractControl<any, any> | null) {
+    if (control?.hasError('phoneExist')) {
+      return 'Phonenumber entered already exist';
+    }
+
+    return '';
   }
 }
