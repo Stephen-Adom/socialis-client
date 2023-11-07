002928c2c284dd4730cdb2c920b8472557e68ff0
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-11-07 21:11:17 +0000

    list all user following on the side section on the dashboard

--- src/lib/followers/src/lib/followers/followers.component.ts
+++ src/lib/followers/src/lib/followers/followers.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+/* eslint-disable @nx/enforce-module-boundaries */
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserState, getAllAuthUserFollowing } from 'state';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { UserSummaryInfo } from 'utils';
 
 @Component({
   selector: 'lib-followers',
@@ -8,4 +14,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './followers.component.html',
   styleUrls: ['./followers.component.css'],
 })
-export class FollowersComponent {}
+export class FollowersComponent implements OnInit {
+  userFollowing$!: Observable<UserSummaryInfo[]>;
+
+  constructor(private store: Store<UserState>, private router: Router) {}
+
+  ngOnInit(): void {
+    this.userFollowing$ = this.store.select(getAllAuthUserFollowing);
+  }
+
+  viewDetails(following: UserSummaryInfo) {
+    this.router.navigate(['user', following.username, 'profile']);
+  }
+}
