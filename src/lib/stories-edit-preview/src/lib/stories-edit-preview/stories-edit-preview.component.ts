5d91fd306872096b80e1f1b02e3ff17e15acb6fa
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-12-20 20:44:29 +0000

    add swiper js to stories preview

--- src/lib/stories-edit-preview/src/lib/stories-edit-preview/stories-edit-preview.component.ts
+++ src/lib/stories-edit-preview/src/lib/stories-edit-preview/stories-edit-preview.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'lib-stories-edit-preview',
   standalone: true,
   imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './stories-edit-preview.component.html',
   styleUrls: ['./stories-edit-preview.component.css'],
 })
