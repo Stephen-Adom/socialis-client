7e1b05231eb21bf3c9ea45720c56a4eb3e6e36b5
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-12-21 17:23:09 +0000

    add responsiveness to stories-dialog components

--- src/shared/services/src/lib/stories-edit-preview.service.ts
+++ src/shared/services/src/lib/stories-edit-preview.service.ts
@@ -5,7 +5,7 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class StoriesEditPreviewService {
-  storiesEditPreview$ = new BehaviorSubject<boolean>(true);
+  storiesEditPreview$ = new BehaviorSubject<boolean>(false);
 
   storiesEditPreviewObservable = this.storiesEditPreview$.asObservable();
 
