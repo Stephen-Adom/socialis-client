2e6ec7c52bde60629c7a730105c36a56b2f909cd
Stephen-Adom <stephen.alaska94@hotmail.com>
2024-01-03 18:00:10 +0000

    design repost container

--- src/lib/repost-card/src/lib/repost-card/repost-card.component.ts
+++ src/lib/repost-card/src/lib/repost-card/repost-card.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+/* eslint-disable @nx/enforce-module-boundaries */
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PostState, getAllPosts } from 'state';
+import { Store } from '@ngrx/store';
+import { Observable, map } from 'rxjs';
+import { PostType } from 'utils';
+import { PostCardComponent } from 'post-card';
 
 @Component({
   selector: 'lib-repost-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PostCardComponent],
   templateUrl: './repost-card.component.html',
   styleUrls: ['./repost-card.component.css'],
 })
-export class RepostCardComponent {}
+export class RepostCardComponent implements OnInit {
+  firstPost$!: Observable<PostType>;
+
+  constructor(private store: Store<PostState>) {}
+
+  ngOnInit(): void {
+    this.firstPost$ = this.store
+      .select(getAllPosts)
+      .pipe(map((posts) => posts[0]));
+  }
+}
